20c08be1b3d92fb992f54579b0f71a2eb1bb10d7
Conortine <89186631+Conortine@users.noreply.github.com>
2021-08-23 16:01:52 +0900

    Name까지
    ~Name

--- src/app.js
+++ src/app.js
@@ -1,3 +1,12 @@
 export function attack(a, b) {
   return a.atk - b.def;
+describe('attack', () => {
+  const a = { atk: 20 };
+  const b = { def: 10 };
+  const damage = 10;
+
+  it('a의 공격력과 b의 방어력을 계산해 데미지를 리턴합니다.', () => {
+    expect(attack(a, b)).toBe(damage);
+  });
+});
 }
